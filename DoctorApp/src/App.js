bfe578c942b82e5b894c8d051d5f8f462c3cc8cd
Sturgen <dodi.alexsander@gmail.com>
2020-07-15 05:06:30 +0700

    default

---
+++ DoctorApp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>asasa</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
