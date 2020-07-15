f3f380b26bef8ed10fff1c10ae0851012f909126
Sturgen <dodi.alexsander@gmail.com>
2020-07-15 18:37:20 +0700

    setup navigator

--- DoctorApp/src/App.js
+++ DoctorApp/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { GetStarted, Splash } from './pages';
 import { NavigationContainer } from '@react-navigation/native';
+import Router from './router';
 
 const App = () => {
   return (
     <NavigationContainer>
-      {/* <GetStarted/> */}
-      <Splash/>
+      <Router/>
     </NavigationContainer>
   )
 }
